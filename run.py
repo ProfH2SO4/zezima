add: he init, gradient clipping, fix: model to double precision cuz GPU arithmetics

run.py
@@ -114,15 +114,26 @@ def main() -> None:
         for f in os.listdir(test_directory)
         if f.endswith(".txt")
     ]
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    if torch.cuda.is_available():
+        log.info(f"CUDA is available. Using {device}.")
+        print(f"CUDA is available. Using {device}.")
+    else:
+        log.info(f"CUDA is not available. Using {device}.")
+        print(f"CUDA is not available. Using {device}.")
 
     for file in train_files:
         model, data_loader, criterion, state_matrix = setup_model_data_loader(
             file, parsed_config
         )
-
+        model.to(device)
+        state_matrix = state_matrix.to(device)
+        model.double()
         if TRAIN_MODE:
             log.info(f"Training model on {file}")
-            optimizer = optim.Adam(model.parameters())
+            optimizer = optim.Adam(
+                model.parameters(), lr=parsed_config["LEARNING_RATE"]
+            )
             train_model(
                 model,
                 criterion,
@@ -131,6 +142,7 @@ def main() -> None:
                 state_matrix,
                 parsed_config["NUM_EPOCHS"],
                 parsed_config["MODEL_PATH"],
+                device,
             )
 
         if VALIDATE_MODE:

zezima/training/train.py
@@ -1,5 +1,6 @@
 import time
 import torch
+import torch.nn.utils as torch_utils
 from torch import Tensor
 
 from zezima import log
@@ -15,6 +16,8 @@ def train_model(
     state_matrix: Tensor,
     num_epochs: int,
     model_path: str,
+    device,
+    max_grad_norm: float = 1.0,
 ):
     start_time = time.time()
 
@@ -30,6 +33,7 @@ def train_model(
             if state_matrix.shape[1] != seq_len:
                 state_matrix = state_matrix[:, :seq_len, :]
             inputs, targets = batch
+            inputs, targets = inputs.to(device), targets.to(device)
             optimizer.zero_grad()
             output, output_state_matrix = model(inputs, state_matrix.detach())
             state_matrix = output_state_matrix
@@ -39,11 +43,21 @@ def train_model(
 
             # Backward pass and optimize
             loss.backward()
+
+            torch_utils.clip_grad_norm_(model.parameters(), max_grad_norm)
             optimizer.step()
 
+            if torch.isnan(loss).any():
+                print("Loss is NaN")
+            formatted_loss = f"{loss.item():.5f}"
+
             if (batch_idx + 1) % 100 == 0:
+                for name, param in model.named_parameters():
+                    if param.requires_grad:
+                        # Log the gradient; you can use any logging method here
+                        log.debug(f"Gradient of {name}: {param.grad}")
                 log.debug(
-                    f"Epoch {epoch + 1}/{num_epochs}, Batch {batch_idx + 1}/{len(data_loader)}, Batch Loss: {loss.item()}"
+                    f"Epoch {epoch + 1}/{num_epochs}, Batch {batch_idx + 1}/{len(data_loader)}, Batch Loss: {formatted_loss}"
                 )
 
         average_loss = total_loss / len(data_loader)

zezima/utils/dataloader.py
@@ -72,10 +72,8 @@ class LimitedDataset(Dataset):
         return len(self.data)
 
     def __getitem__(self, idx: int):
-        # Calculate the end index of the batch
         end_idx = min(idx + self.bp_per_batch, len(self.data))
 
-        # Check if the index is within bounds
         if idx >= len(self.data):
             raise IndexError("Index out of bounds")
 
@@ -83,15 +81,14 @@ class LimitedDataset(Dataset):
         targets_sequence: list[list[int]] = [
             read_boundary_line(i, self.positions_to_look) for i in inputs_sequence
         ]
-
-        return torch.tensor(inputs_sequence, dtype=torch.float32), torch.tensor(
-            targets_sequence, dtype=torch.float32
+        return torch.tensor(inputs_sequence, dtype=torch.float64), torch.tensor(
+            targets_sequence, dtype=torch.float64
         )
 
     def convert_vector_to_torch(self, vector):
         # Process each element in the vector. If an element is a list, convert it to a tensor
         return [
-            torch.tensor(sub_vector, dtype=torch.float32)
+            torch.tensor(sub_vector, dtype=torch.float64)
             if isinstance(sub_vector, list)
             else sub_vector
             for sub_vector in vector